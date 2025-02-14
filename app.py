69e9babc9cc0b11e4241a163e9a80c5c216358c0
Anuj Saini <sainianuj1235@gmail.com>
2025-02-14 18:27:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,5 +72,5 @@ def extract_features(url):
     return features
 
 # Run the app
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
